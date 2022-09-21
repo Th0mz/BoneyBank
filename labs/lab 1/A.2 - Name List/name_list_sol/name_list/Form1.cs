425be829a021930a67ef8317b8d546a09a3602d0
Tomás Tavares <57302012+Th0mz@users.noreply.github.com>
2022-09-21 19:05:28 +0100

    setup repo

---
+++ labs/lab 1/A.2 - Name List/name_list_sol/name_list/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace name_list
+{
+    public partial class Form1 : Form
+    {
+        NameList names = new NameList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            textBox1.Text = "";
+
+            if (text == "")
+            {
+                MessageBox.Show("Invalid name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // add element to name list
+            names.addName(text);
+
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+            string[] list = names.getNames();
+            listBox1.Items.Clear();
+
+            foreach (string name in list)
+            {
+                listBox1.Items.Add(name);
+            }
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            // erase method
+            names.eraseNames();
+        }
+    }
+}
---
+++ labs/lab 1/B.2 - Chat/DADChat/ChatClient/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Grpc.Net.Client;
+
+namespace ChatClient {
+    public partial class Form1 : Form {
+        private GrpcChannel channel;
+        private ChatServerService.ChatServerServiceClient client;
+        public Form1() {
+            InitializeComponent();
+
+            AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            channel = GrpcChannel.ForAddress("http://localhost:1001");
+            client = new ChatServerService.ChatServerServiceClient(channel);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var reply = client.Register(
+                         new ChatClientRegisterRequest { Nick = textBox2.Text, Url = "http://localhost:....." });
+            textBox1.Text = reply.Ok.ToString();
+        }
+    }
+}
