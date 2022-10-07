3d89d198ac0ab15670bd7d823b983a42916d61dc
Tomás Tavares <57302012+Th0mz@users.noreply.github.com>
2022-10-07 17:07:16 +0100

    basic server gRPC service implementation class done

---
+++ BoneyBank/Boney/implementations/CompareAndSwapImpl.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Threading.Channels;
+using Grpc.Net.Client;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.CompilerServices;
+
+public class CompareAndSwapImpl : CompareAndSwapService.CompareAndSwapServiceBase {
+
+    public CompareAndSwapImpl() {
+        AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+    }
+
+    // compareAndSwap funcionality
+    public override Task<CompareAndSwapReply> CompareAndSwap 
+        (CompareAndSwapRequest request, ServerCallContext context) {
+
+        return Task.FromResult(do_compareAndSwap(request));
+    }
+
+    private CompareAndSwapReply do_compareAndSwap(CompareAndSwapRequest request) {
+        return new CompareAndSwapReply();
+    }
+}
---
+++ BoneyBank/Boney/implementations/PaxosImpl.cs
@@ -0,0 +1,39 @@
+﻿using Grpc.Core;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Threading.Channels;
+using Grpc.Net.Client;
+using static System.Net.Mime.MediaTypeNames;
+
+public class PaxosImpl : PaxosService.PaxosServiceBase {
+
+    public PaxosImpl() {
+        AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+    }
+
+    // accept funcionality
+    public override Task<AcceptReply> Accept
+        (AcceptRequest request, ServerCallContext context) {
+
+        return Task.FromResult(do_accept(request));
+    }
+
+    private AcceptReply do_accept(AcceptRequest request) {
+        // accept code
+        return new AcceptReply();
+    }
+
+
+    // keepalive funcionality
+    public override Task<KeepaliveReply> Keepalive
+        (KeepaliveRequest request, ServerCallContext context) {
+
+        return Task.FromResult(do_keepalive(request));
+    }
+
+    private KeepaliveReply do_keepalive(KeepaliveRequest request) {
+        // keepalive code
+        return new KeepaliveReply();
+    }
+}
